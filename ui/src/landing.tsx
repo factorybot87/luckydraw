7142a8e9e860157c6f5424d370545bf42fd7b1f9
Eric <eric.liao@access-company.com>
2020-12-21 00:26:32 +0800

    init startup (babel, webpack, typeScript,react )

---
+++ ui/src/landing.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const a = { a: 122 }
+console.log(a)
+const App = () => <h1>React + TypeScript + Webpack test123</h1>
+
+ReactDOM.render(<App />, document.getElementById('root'))
