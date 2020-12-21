fa7d883bbf6737d71bddc90c13b2c81899cd14e3
Jerry Guo <jerry.guo@access-company.com>
2020-12-21 17:53:19 +0800

    setup docker

--- lucky_draw/config/dev.exs
+++ lucky_draw/config/dev.exs
@@ -5,7 +5,7 @@ config :lucky_draw, LuckyDraw.Repo,
   username: "postgres",
   password: "postgres",
   database: "lucky_draw_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
