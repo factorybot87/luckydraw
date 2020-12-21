dd35ca99d1691b01b8ac5b98ab7c45a26761ee50
Eric <eric.liao@access-company.com>
2020-12-21 11:29:53 +0800

    remove log

--- ui/src/landing.tsx
+++ ui/src/landing.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const a = { a: 122 }
-console.log(a)
+
 const App = () => <h1>React + TypeScript + Webpack Test</h1>
 
 ReactDOM.render(<App />, document.getElementById('root'))
