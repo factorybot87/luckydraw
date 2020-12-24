1ab24a451f835569431f171b71a804d09819c6a2
Jerry Guo <jerry.guo@access-company.com>
2020-12-24 14:59:01 +0800

    add GraphiQL playground

--- lucky_draw/lib/lucky_draw_web/router.ex
+++ lucky_draw/lib/lucky_draw_web/router.ex
@@ -23,5 +23,12 @@ defmodule LuckyDrawWeb.Router do
       pipe_through [:fetch_session, :protect_from_forgery]
       live_dashboard "/dashboard", metrics: LuckyDrawWeb.Telemetry
     end
+
+    scope "/graphiql" do
+      forward "/", Absinthe.Plug.GraphiQL,
+        schema: LuckyDrawWeb.Schema,
+        interface: :playground,
+        context: %{pubsub: LuckyDrawWeb.Endpoint}
+    end
   end
 end
