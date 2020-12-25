2d7ee3ec7b5221abaa3cdc99174509de7c4da7bf
Jerry Guo <jerry.guo@access-company.com>
2020-12-25 15:07:09 +0800

    add list_award and create_award test

--- lucky_draw/test/lucky_draw/lottery_test.exs
+++ lucky_draw/test/lucky_draw/lottery_test.exs
@@ -6,8 +6,7 @@ defmodule LuckyDraw.LotteryTest do
   describe "awards" do
     alias LuckyDraw.Lottery.Award
 
-    @valid_attrs %{}
-    @update_attrs %{}
+    @valid_attrs %{content: "Clean Code", price: 580, provider_name: "Mike Chen"}
     @invalid_attrs %{}
 
     def award_fixture(attrs \\ %{}) do
@@ -19,10 +18,17 @@ defmodule LuckyDraw.LotteryTest do
       award
     end
 
-    @tag :skip
     test "list_awards/0 returns all awards" do
       award = award_fixture()
       assert Lottery.list_awards() == [award]
+    end
+
+    test "create_award/1 with valid data creates a award" do
+      assert {:ok, %Award{} = award} = Lottery.create_award(@valid_attrs)
+    end
+
+    test "create_award/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Lottery.create_award(@invalid_attrs)
     end
   end
 end
