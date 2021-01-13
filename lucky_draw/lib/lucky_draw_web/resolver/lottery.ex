c57cb6c008dc989b32de7ee3ab8a3a77aba32143
Jerry Guo <jerry.guo@access-company.com>
2021-01-13 14:36:04 +0800

    define schema resolver

--- lucky_draw/lib/lucky_draw_web/resolver/lottery.ex
+++ lucky_draw/lib/lucky_draw_web/resolver/lottery.ex
@@ -1,5 +1,27 @@
+alias LuckyDraw.Lottery
+
 defmodule LuckyDrawWeb.Resolver.Lottery do
   def list_awards(_parent, _args, _resolution) do
-    {:ok, LuckyDraw.Lottery.list_awards()}
+    {:ok, Lottery.list_awards()}
+  end
+
+  def get_award(_parent, %{id: id}, _resolution) do
+    {:ok, Lottery.get_award!(id)}
+  end
+
+  def create_award(_parent, args, _resolution) do
+    Lottery.create_award(args)
+  end
+
+  def update_award(_parent, %{id: id} = args, _resolution) do
+    id
+    |> Lottery.get_award!()
+    |> Lottery.update_award(args)
+  end
+
+  def delete_award(_parent, %{id: id}, _resolution) do
+    id
+    |> Lottery.get_award!()
+    |> Lottery.delete_award()
   end
 end
