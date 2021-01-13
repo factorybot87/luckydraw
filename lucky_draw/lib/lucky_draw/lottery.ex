a7422b53423a4dd5dbaad2399930d124a5e79405
Jerry Guo <jerry.guo@access-company.com>
2021-01-13 14:34:25 +0800

    award CRUD without C

--- lucky_draw/lib/lucky_draw/lottery.ex
+++ lucky_draw/lib/lucky_draw/lottery.ex
@@ -8,9 +8,21 @@ defmodule LuckyDraw.Lottery do
     Repo.all(Award)
   end
 
+  def get_award!(id), do: Repo.get!(Award, id)
+
   def create_award(attrs \\ %{}) do
     %Award{}
     |> Award.changeset(attrs)
     |> Repo.insert()
+  end
+
+  def update_award(%Award{} = award, attrs) do
+    award
+    |> Award.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_award(%Award{} = award) do
+    Repo.delete(award)
   end
 end
