86eaaf32286278cbe41fded0ed88551ef863f2d5
Jerry Guo <jerry.guo@access-company.com>
2021-01-14 10:58:23 +0800

    add lottery context

--- lucky_draw/lib/lucky_draw/lottery.ex
+++ lucky_draw/lib/lucky_draw/lottery.ex
@@ -3,12 +3,15 @@ defmodule LuckyDraw.Lottery do
   alias LuckyDraw.Repo
 
   alias LuckyDraw.Lottery.Award
+  alias LuckyDraw.Lottery.Candidate
 
   def list_awards do
     Repo.all(Award)
   end
 
-  def get_award!(id), do: Repo.get!(Award, id)
+  def get_award!(id) do
+    Repo.get!(Award, id)
+  end
 
   def create_award(attrs \\ %{}) do
     %Award{}
@@ -24,5 +27,19 @@ defmodule LuckyDraw.Lottery do
 
   def delete_award(%Award{} = award) do
     Repo.delete(award)
+  end
+
+  def list_candidates do
+    Repo.all(Candidate)
+  end
+
+  def get_candidate!(id) do
+    Repo.get!(Candidate, id)
+  end
+
+  def update_candidate(%Candidate{} = candidate, attrs) do
+    candidate
+    |> Candidate.changeset(attrs)
+    |> Repo.update()
   end
 end
