728445b0c594724a224ca1a04168556246a08466
Yi-Shin Ho <yishin.ho@access-company.com>
2021-01-15 16:29:47 +0800

    add: add AwardForm

--- ui/src/components/Input/Input.tsx
+++ ui/src/components/Input/Input.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import style from './style.scss'
 
+const PLACEHOLDER = '感恩讚乾爹乾媽'
+
 interface Props {
   name: string
-  value: any
+  onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+  error?: string
+  value?: any
   type?: string
   label?: string
-  onChange?: () => void
+  placeholder?: string
 }
 
-export default function Input({ name, value, type = 'text', label, onChange }: Props) {
+export default function Input({
+  name,
+  error,
+  value,
+  type = 'text',
+  placeholder = PLACEHOLDER,
+  label,
+  onChange
+}: Props) {
   return (
-    <div className={style.input}>
+    <div className={style.inputContainer}>
       {label && <label>{label}</label>}
-      <input name={name} type={type} value={value} onChange={onChange} />
+      <div className={style.input}>
+        <input name={name} type={type} value={value} onChange={(e) => onChange(e)} placeholder={placeholder} />
+        {error && <div className={style.error}>{error}</div>}
+      </div>
     </div>
   )
 }
