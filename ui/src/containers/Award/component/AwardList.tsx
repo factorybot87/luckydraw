28bd518d074f7e3b37b36f619b5b2b01c173cd51
Stanley Liang <stanley.liang@access-company.com>
2021-01-19 13:44:40 +0800

    display winner name in awards list instead of id (#22)
    * display winner name in awards list instead of id
    
    * fixup! display winner name in awards list instead of id

--- ui/src/containers/Award/component/AwardList.tsx
+++ ui/src/containers/Award/component/AwardList.tsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useQuery } from '@apollo/client'
 
 import style from './AwardListStyle.scss'
 import logo from '../../../assets/access-logo.png'
 import AwardItem from './AwardItem'
 import { AWARD_LIST_QUERY } from '../../../query/award'
+import { CANDIDATE_LIST_QUERY } from '../../../query/candidates'
 import { AwardsList } from '../../../__generated__/AwardsList'
+import { CandidatesList } from '../../../__generated__/CandidatesList'
 import AwardCreation from './AwardCreation'
+import AwardDerived from './AwardDerived'
 
 function AwardList() {
   const { data: awardsListData } = useQuery<AwardsList>(AWARD_LIST_QUERY)
-
+  const { data: candidatesListData } = useQuery<CandidatesList>(CANDIDATE_LIST_QUERY)
+  const awardsData: AwardDerived[] = useMemo(
+    () =>
+      awardsListData?.awards?.map((award: any) => {
+        const candidateName =
+          candidatesListData?.candidates?.find((candidateToCheck: any) => candidateToCheck.id === award.winner)?.name ??
+          'unknown'
+        return {
+          ...award,
+          winnerName: candidateName
+        }
+      }) ?? [],
+    [awardsListData, candidatesListData]
+  )
   return (
     <div className={style.container}>
       <div className={style.header}>
@@ -21,7 +37,7 @@ function AwardList() {
       </div>
       <div className={style.content}>
         <AwardCreation />
-        {awardsListData?.awards?.map((e) => (
+        {awardsData.map((e) => (
           <AwardItem key={e.id} award={e} />
         ))}
       </div>
